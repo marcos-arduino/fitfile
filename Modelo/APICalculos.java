2e53ba1b223910e8b5a683fca850df80692b4a2d
marcos-arduino <137747983+marcos-arduino@users.noreply.github.com>
2024-11-21 21:11:13 -0300

    Update APICalculos.java

--- Modelo/APICalculos.java
+++ Modelo/APICalculos.java
@@ -5,5 +5,16 @@ public class APICalculos {
         return peso / (altura * altura);
     }
 
-    // Otros métodos para calcular diferentes índices...
+    public static double calcularGrasaCorporal(double imc, int edad, boolean esHombre) {
+        double factorGenero = esHombre ? 1.0 : 0.0;
+        return (1.20 * imc) + (0.23 * edad) - (10.8 * factorGenero) - 5.4;
+    }
+
+    public static double calcularTMB(double peso, double altura, int edad, boolean esHombre) {
+        if (esHombre) {
+            return 66 + (13.75 * peso) + (5 * altura * 100) - (6.75 * edad);
+        } else {
+            return 655 + (9.56 * peso) + (1.85 * altura * 100) - (4.68 * edad);
+        }
+    }
 }
