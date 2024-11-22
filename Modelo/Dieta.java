e6b6ae083184d29883c24814a59dd4ba1b65b4d3
marcos-arduino <137747983+marcos-arduino@users.noreply.github.com>
2024-11-21 21:10:59 -0300

    Update Dieta.java

--- Modelo/Dieta.java
+++ Modelo/Dieta.java
@@ -6,21 +6,48 @@ import java.util.List;
 public class Dieta {
     private List<String> platos;
     private List<String> ideasIA;
+    private int totalKcal;
+    private int totalProteinas;
+    private int totalCarbohidratos;
+    private int totalGrasas;
 
     // Constructor
     public Dieta() {
         this.platos = new ArrayList<>();
         this.ideasIA = new ArrayList<>();
+        this.totalKcal = 0;
+        this.totalProteinas = 0;
+        this.totalCarbohidratos = 0;
+        this.totalGrasas = 0;
     }
 
-    // Métodos para manejar los platos y las ideas generadas por IA
-    public void agregarPlato(String plato) {
+    // MÃ©todos para manejar los platos y las ideas generadas por IA
+    public void agregarPlato(String plato, int kcal, int proteinas, int carbohidratos, int grasas) {
         this.platos.add(plato);
+        this.totalKcal += kcal;
+        this.totalProteinas += proteinas;
+        this.totalCarbohidratos += carbohidratos;
+        this.totalGrasas += grasas;
     }
 
     public void agregarIdeaIA(String idea) {
         this.ideasIA.add(idea);
     }
 
-    // Otros getters y setters...
+    // Getters para los totales
+    public int getTotalKcal() {
+        return totalKcal;
+    }
+
+    public int getTotalProteinas() {
+        return totalProteinas;
+    }
+
+    public int getTotalCarbohidratos() {
+        return totalCarbohidratos;
+    }
+
+    public int getTotalGrasas() {
+        return totalGrasas;
+    }
 }
