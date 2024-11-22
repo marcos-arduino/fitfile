8d401ecd4ff770eb372cb905169bcda560b55082
marcos-arduino <137747983+marcos-arduino@users.noreply.github.com>
2024-11-21 21:09:03 -0300

    Update Rutina.java

--- Modelo/Rutina.java
+++ Modelo/Rutina.java
@@ -18,10 +18,41 @@ public class Rutina {
         this.ejercicios = new ArrayList<>();
     }
 
-    // Métodos para manejar los ejercicios en la rutina
+    // MÃ©todos para manejar los ejercicios en la rutina
     public void agregarEjercicio(Ejercicio ejercicio) {
         this.ejercicios.add(ejercicio);
     }
 
-    // Otros getters y setters...
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public List<Ejercicio> getEjercicios() {
+        return new ArrayList<>(ejercicios);
+    }
+
+    public void setEjercicios(List<Ejercicio> ejercicios) {
+        this.ejercicios = ejercicios;
+    }
 }
